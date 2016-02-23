fd0759d4a211e44b2d981d2df3b7f2e312d90aae
Srđan Milaković <srki@outlook.com>
2016-02-23 21:24:39 +0100

    Added database schema.

--- database_script/StructureOnly.sql
+++ database_script/StructureOnly.sql
@@ -18,23 +18,6 @@ USE `restaurant_management`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `surname` varchar(50) NOT NULL,
-  `visits` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_role` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `friend`
 --
 
@@ -42,12 +25,12 @@ DROP TABLE IF EXISTS `friend`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friend` (
-  `following_id` int(11) NOT NULL,
-  `followed_id` int(11) NOT NULL,
-  PRIMARY KEY (`following_id`,`followed_id`),
-  KEY `fk_friend2` (`followed_id`),
-  CONSTRAINT `fk_friend` FOREIGN KEY (`following_id`) REFERENCES `customer` (`id`),
-  CONSTRAINT `fk_friend2` FOREIGN KEY (`followed_id`) REFERENCES `customer` (`id`)
+  `following` int(11) NOT NULL,
+  `followed` int(11) NOT NULL,
+  PRIMARY KEY (`following`,`followed`),
+  KEY `fk_friend2` (`followed`),
+  CONSTRAINT `fk_friend` FOREIGN KEY (`following`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_friend2` FOREIGN KEY (`followed`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -59,13 +42,15 @@ DROP TABLE IF EXISTS `friend_rating`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friend_rating` (
-  `customer_user_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `restaurant_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `sum` int(11) NOT NULL DEFAULT '0',
   `count` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`customer_user_id`,`restaurant_id`),
-  KEY `fk_restaurant_friend_rating` (`restaurant_id`),
-  CONSTRAINT `fk_customer_friend_rating` FOREIGN KEY (`customer_user_id`) REFERENCES `customer` (`id`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ak_identifier_2` (`restaurant_id`,`user_id`),
+  KEY `fk_customer_friend_rating` (`user_id`),
+  CONSTRAINT `fk_customer_friend_rating` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_restaurant_friend_rating` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -78,16 +63,35 @@ DROP TABLE IF EXISTS `invitation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `invitation` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `reservation_id` int(11) NOT NULL,
-  `customer_user_id` int(11) NOT NULL,
-  `accepted` smallint(6) NOT NULL,
+  `accepted` tinyint(1) DEFAULT NULL,
   `rating` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_invitation` (`reservation_id`),
-  KEY `fk_invited` (`customer_user_id`),
+  KEY `fk_invited` (`user_id`),
   CONSTRAINT `fk_invitation` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`),
-  CONSTRAINT `fk_invited` FOREIGN KEY (`customer_user_id`) REFERENCES `customer` (`id`)
+  CONSTRAINT `fk_invited` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_has_log` (`user_id`),
+  CONSTRAINT `fk_has_log` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -99,10 +103,10 @@ DROP TABLE IF EXISTS `meal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `meal` (
-  `name` varchar(50) DEFAULT NULL,
-  `description` longtext,
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `restaurant_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text,
   `price` double NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_has` (`restaurant_id`),
@@ -118,14 +122,14 @@ DROP TABLE IF EXISTS `oauth2_account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `oauth2_account` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `social_network` char(2) NOT NULL,
-  `user_id` varchar(50) NOT NULL,
+  `client_id` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `ak_identifier_2` (`social_network`,`user_id`),
-  KEY `fk_has_social_account` (`customer_id`),
-  CONSTRAINT `fk_has_social_account` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
+  UNIQUE KEY `ak_identifier_2` (`social_network`,`client_id`),
+  KEY `fk_has_social_account` (`user_id`),
+  CONSTRAINT `fk_has_social_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -137,17 +141,17 @@ DROP TABLE IF EXISTS `reservation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reservation` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `restaurant_id` int(11) NOT NULL,
-  `customer_user_id` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `length` smallint(6) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_makes_reservation` (`customer_user_id`),
+  KEY `fk_makes_reservation` (`user_id`),
   KEY `fk_reserved_in` (`restaurant_id`),
-  CONSTRAINT `fk_makes_reservation` FOREIGN KEY (`customer_user_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `fk_makes_reservation` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_reserved_in` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -175,33 +179,18 @@ DROP TABLE IF EXISTS `restaurant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `restaurant` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `description` longtext,
+  `description` text,
   `rating_sum` int(11) NOT NULL DEFAULT '0',
   `rating_count` int(11) NOT NULL DEFAULT '0',
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `rows` smallint(6) NOT NULL DEFAULT '0',
+  `columns` smallint(6) NOT NULL DEFAULT '0',
+  `_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `session_token`
---
-
-DROP TABLE IF EXISTS `session_token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `session_token` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `value` char(128) NOT NULL,
-  `expiration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `fk_session` (`user_id`),
-  CONSTRAINT `fk_session` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,14 +201,33 @@ DROP TABLE IF EXISTS `table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `table` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `restaurant_id` int(11) NOT NULL,
   `label` varchar(5) DEFAULT NULL,
   `row` int(11) NOT NULL,
   `column` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `position_unique` (`restaurant_id`,`row`,`column`),
+  UNIQUE KEY `ak_identifier_2` (`restaurant_id`,`row`,`column`),
   CONSTRAINT `fk_contains` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `value` char(128) NOT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  `type` char(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_has_token` (`user_id`),
+  CONSTRAINT `fk_has_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -231,16 +239,20 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `restaurant_id` int(11) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(25) NOT NULL,
   `role` varchar(25) NOT NULL,
-  `activated` smallint(6) NOT NULL,
+  `activated` tinyint(1) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `visits` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_is_manager` (`restaurant_id`),
   CONSTRAINT `fk_is_manager` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -252,4 +264,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-14 13:51:20
+-- Dump completed on 2016-02-23 21:20:36
